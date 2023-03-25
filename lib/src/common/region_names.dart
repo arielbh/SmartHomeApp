4f174a6e30040cdd3021d74d0a740853c02ecfa1
Ariel Ben Horesh <ariel@lumen.me>
2023-03-25 12:14:53 +0300

    Add Regions

---
+++ lib/src/common/region_names.dart
@@ -0,0 +1,7 @@
+enum RegionNames {
+  topBarLeft,
+  topBarRight,
+  mainLeft,
+  mainCenter,
+  mainRight,
+}
--- lib/src/modules/home/views/layout_widget.dart
+++ lib/src/modules/home/views/layout_widget.dart
@@ -1,10 +1,61 @@
 import 'package:flutter/cupertino.dart';
+import 'package:prism_flutter_core/prism_flutter_core.dart';
+import 'package:smart_home_app/src/common/dependencies/app_locator.dart';
+import 'package:smart_home_app/src/common/region_names.dart';
 
 class LayoutWidget extends StatelessWidget {
   const LayoutWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Text("Margol");
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            RegionBuilder(
+              regionManager: locator<RegionManager>(),
+              regionName: RegionNames.topBarLeft.name,
+              multiChild: (children) => Row(
+                children: [
+                  ...children,
+                ],
+              ),
+            ),
+            RegionBuilder(
+              regionManager: locator<RegionManager>(),
+              regionName: RegionNames.topBarRight.name,
+              multiChild: (children) => Row(
+                children: [
+                  ...children,
+                ],
+              ),
+            ),
+          ],
+        ),
+        Expanded(
+            child: Row(
+          children: [
+            RegionBuilder(
+              regionManager: locator<RegionManager>(),
+              regionName: RegionNames.mainLeft.name,
+              singleChild: (child) => child,
+            ),
+            Expanded(
+              child: RegionBuilder(
+                regionManager: locator<RegionManager>(),
+                regionName: RegionNames.mainCenter.name,
+                singleChild: (child) => child,
+              ),
+            ),
+            RegionBuilder(
+              regionManager: locator<RegionManager>(),
+              regionName: RegionNames.mainRight.name,
+              singleChild: (child) => child,
+            ),
+          ],
+        ))
+      ],
+    );
   }
 }
