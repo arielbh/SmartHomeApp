87e2b81fbddac8b81e0145afda8ad40e47078278
Ariel Ben Horesh <ariel@lumen.me>
2023-05-07 23:46:11 +0300

    Welcome Screen

--- lib/src/modules/user/views/welcome_widget.dart
+++ lib/src/modules/user/views/welcome_widget.dart
@@ -7,17 +7,49 @@ class WelcomeWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        children: [
-          const Text('Welcome'),
-          TextButton(
-              onPressed: () {
-                locator<GoRouter>().go("/layout");
-              },
-              child: const Text("Next1")),
-        ],
-      ),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Image.asset("assets/images/welcome.png"),
+        Expanded(
+          child: Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20.0),
+              color: Colors.grey[300],
+            ),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20.0),
+              child: Column(
+                children: [
+                  const SizedBox(height: 10.0),
+                  Text('Welcome', style: Theme.of(context).textTheme.headlineSmall),
+                  Text('Smart Home', style: Theme.of(context).textTheme.headlineMedium),
+                  const SizedBox(height: 10.0),
+                  const TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: 'Enter your email',
+                    ),
+                  ),
+                  const SizedBox(height: 10.0),
+                  const TextField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: 'Enter your password',
+                    ),
+                  ),
+                  TextButton(
+                      onPressed: () {
+                        locator<GoRouter>().go("/layout");
+                      },
+                      child: const Text("Continue")),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
