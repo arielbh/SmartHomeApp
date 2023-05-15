b5c88bfc7f879c73b592e31e70ec057ed14c570f
Ariel Ben Horesh <ariel@lumen.me>
2023-05-15 17:34:37 +0200

    Remove Shared Pref

--- lib/src/modules/app_services/app_services_module.dart
+++ lib/src/modules/app_services/app_services_module.dart
@@ -1,14 +1,10 @@
 import 'package:get_it/get_it.dart';
 import 'package:prism_flutter_getit/prism_flutter_getit.dart';
-import 'package:smart_home_app/src/modules/app_services/services/shared_preferences_service.dart';
 import 'package:smart_home_app/src/modules/app_services/services/toaster.dart';
 
 class AppServicesModule extends GetItModule {
   @override
   Future<void> init(GetIt container) async {
-    final storage = SharedPreferencesLocalStorageService();
-    await storage.initialize();
-    container.registerSingleton<LocalStorageService>(storage);
     container.registerLazySingleton<ToasterService>(() => ToasterService());
     container.registerSingleton(Toaster());
   }
